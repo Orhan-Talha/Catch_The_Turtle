45d1ea7a8004a16e8f5728f039e2d11a88d46840
Orhan Talha Çetinel <cetinelorhantalha08@gmail.com>
2023-07-26 20:52:35 +0300

    Added turtle to screen

--- Catch The Turtle Oyunu.py
+++ Catch The Turtle Oyunu.py
@@ -1,6 +1,7 @@
 import turtle
 s=turtle.Screen()
 s.bgcolor("lightblue")
+s.title("Catch The Turtle")
 sc=0
 score="score = {}"
 txt = turtle.Turtle()
@@ -11,6 +12,11 @@ top_height = s.window_height()/2  # positive height/2 is the top of the screen
 y = top_height - top_height/10  # decreasing a little bit so text will be visible
 txt.setposition(0, y)
 txt.write(arg=score.format(sc), move=True, align='center', font=("arial", 20, "normal"))
+turtle1 = turtle.Turtle()
+turtle1.shapesize(stretch_wid=2, stretch_len=2, outline=None)
+turtle1.shape("turtle")
+turtle1.color("green")
+turtle1.penup()
 
 
 
@@ -32,10 +38,7 @@ txt.write(arg=score.format(sc), move=True, align='center', font=("arial", 20, "n
 
 
 
-
-
-
-turtle.done()
+turtle.mainloop()
 
 
 
