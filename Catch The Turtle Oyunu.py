f84598203525944dd464606a21e92aa02ad46cd6
Orhan Talha Çetinel <cetinelorhantalha08@gmail.com>
2023-07-26 20:40:12 +0300

    Added scoreboard to screen

--- Catch The Turtle Oyunu.py
+++ Catch The Turtle Oyunu.py
@@ -1,7 +1,40 @@
 import turtle
 s=turtle.Screen()
 s.bgcolor("lightblue")
-s.title("Catch The Turtle")
+sc=0
+score="score = {}"
+txt = turtle.Turtle()
+txt.color("blue")
+txt.penup()
+txt.ht()
+top_height = s.window_height()/2  # positive height/2 is the top of the screen
+y = top_height - top_height/10  # decreasing a little bit so text will be visible
+txt.setposition(0, y)
+txt.write(arg=score.format(sc), move=True, align='center', font=("arial", 20, "normal"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 turtle.done()
 
 
